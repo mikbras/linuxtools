8d527c2e9c9d9d87b825bd0c91945a7f23438d1d
Mike Brasher <mikbras@microsoft.com>
2024-04-03 23:55:08 +0000

    icleanup

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ git config --global user.email "mikbras@microsoft.com"
 git config --global user.name "Mike Brasher"
 cp -r ./bin ~/bin
 
+sudo apt update
+sudo apt install -y make gcc g++ fio
+
 echo "now type this: source ~/.bashrc"
